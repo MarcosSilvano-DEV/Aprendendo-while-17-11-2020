b1bebb4d8d443b4b29d61684147f6d9fde26a590
Marcos Silvano <marcossilvano@gmail.com>
2020-11-17 20:32:59 -0300

    commit 2 projeto 17-11-2020

--- Program.cs
+++ Program.cs
@@ -6,18 +6,37 @@ namespace Aprendendo_while_17_11_2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Aprendendo WHILE");
-            
-            Console.WriteLine("Digite um número");
-            int numero = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Aprendendo WHILE");
+        //  
+        //    Console.WriteLine("Digite um número");
+        //    int numero = int.Parse(Console.ReadLine());
 
-            int contador = 0;
+        //    int contador = 0;
 
-            while (contador <= numero)
-            {
-                Console.WriteLine(contador);
-                contador = contador +1;
-            };
+        //    while (contador <= numero)
+        //    {
+        //        Console.WriteLine(contador);
+        //        contador = contador +1;
+        //    };
+
+        //    string resposta = "";
+        //    do{
+        //        Console.WriteLine("Digite seu e-mail");
+        //        resposta = Console.ReadLine();
+
+        //    }while(resposta != "exgorfo@gmail.com");
+
+        //    Console.WriteLine("E-mail confirmado");
+              
+              Console.WriteLine("Digite a idade do ator");
+              int idade = int.Parse(Console.ReadLine());
+
+              while(idade != 47)
+              {
+                  Console.WriteLine("Errou!");
+                  Console.WriteLine("Digite a idade do ator");
+                  int idade = int.Parse(Console.ReadLine());
+              }
         }
     }
 }
