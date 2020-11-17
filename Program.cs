9f0c9d0bb79ef935ad360fc4bb576f5819001d96
Marcos Silvano <marcossilvano@gmail.com>
2020-11-17 14:51:28 -0300

    Aprendendo while 17-11-2020

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aprendendo_while_17_11_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendendo WHILE");
+            
+            Console.WriteLine("Digite um número");
+            int numero = int.Parse(Console.ReadLine());
+
+            int contador = 0;
+
+            while (contador <= numero)
+            {
+                Console.WriteLine(contador);
+                contador = contador +1;
+            };
+        }
+    }
+}
